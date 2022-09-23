695921bb134f73f3d281d629e18e3bfe63287913
Andrei Olekh <andr_vol@mail.ru>
2022-09-23 15:17:29 +0300

    Add StringModify class

---
+++ src/StringModify.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Oleh\MyComposerLib;
+
+class StringModify
+{
+    public function convertString($string)
+    {
+        $string = mb_convert_case(mb_strtolower($string),MB_CASE_TITLE, "UTF-8");
+
+        return $string;
+    }
+}
